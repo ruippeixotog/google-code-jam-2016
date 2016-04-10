ad68c61dfef9c176de4edb3387b36cabebd75120
Rui Gonçalves <ruippeixotog@gmail.com>
2016-04-10 18:50:41 +0100

    Clean up Counting Sheep code

--- qualification/counting-sheep.cpp
+++ qualification/counting-sheep.cpp
@@ -1,58 +1,35 @@
-#include <algorithm>
 #include <cstdio>
 #include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <utility>
-#include <vector>
-
-#define MAXN 2000
 
 using namespace std;
 
 typedef long long ll;
-typedef long double ld;
 
 bool found[10];
 
 int main() {
-  int t;
-  scanf("%d", &t);
+  int t; scanf("%d", &t);
   for (int tc = 1; tc <= t; tc++) {
-    int n;
-    scanf("%d", &n);
+    int n; scanf("%d", &n);
 
-    memset(found, 0, sizeof(found));
+    int last = 0;
+    if(n != 0) {
+      memset(found, 0, sizeof(found));
 
-    int i, last;
-    for (i = 1; i < 101; i++) {
-      last = n * i;
-      int res = last;
-      while(res != 0) {
-//        if(!found[res % 10]) fprintf(stderr, "found: %d\n", res % 10);
-        found[res % 10] = true;
-        res /= 10;
-      }
+      bool foundAll = false;
+      while(!foundAll) {
+        int next = last = last + n;
+        while(next != 0) { found[next % 10] = true; next /= 10; }
 
-      bool foundAll = true;
-      for(int j = 0; j < 10; j++) {
-        if(!found[j]) {
-          foundAll = false;
-          break;
+        foundAll = true;
+        for(int j = 0; j < 10; j++) {
+          if(!found[j]) { foundAll = false; break; }
         }
       }
-
-      if(foundAll) break;
     }
 
-    if(i != 101) {
-      printf("Case #%d: %d\n", tc, last);
-    } else {
-      printf("Case #%d: INSOMNIA\n", tc);
-    }
+    if(last == 0) printf("Case #%d: INSOMNIA\n", tc);
+    else printf("Case #%d: %d\n", tc, last);
   }
   return 0;
 }
--- qualification/fractiles.cpp
+++ qualification/fractiles.cpp
@@ -6,8 +6,7 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-  int t;
-  scanf("%d", &t);
+  int t; scanf("%d", &t);
   for (int tc = 1; tc <= t; tc++) {
     int k, c, s; scanf("%d %d %d", &k, &c, &s);
 
