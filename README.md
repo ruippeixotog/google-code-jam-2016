e125ac455ab5f4400640c2126f1ede7d2256a412
Rui Gonçalves <ruippeixotog@gmail.com>
2016-04-10 18:32:05 +0100

    Add full solution for Fractiles

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ The following is the list of the problems solved. Each problem identifier is spe
 * ✓ [A: Counting Sheep][qual1] (`counting-sheep`)
 * ✓ [B: Revenge of the Pancakes][qual2] (`revenge-of-the-pancakes`)
 * ✗ [C: Coin Jam][qual3] (`coin-jam`)
-* ✗ [D: Fractiles][qual4] (`fractiles`)
+* ✓ [D: Fractiles][qual4] (`fractiles`)
 
 [1]: https://code.google.com/codejam
 [qual1]: https://code.google.com/codejam/contest/6254486/dashboard#s=p0
--- qualification/fractiles.cpp
+++ qualification/fractiles.cpp
@@ -1,35 +1,26 @@
 #include <algorithm>
 #include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <utility>
-#include <vector>
-
-#define MAXN 2000
 
 using namespace std;
 
 typedef long long ll;
-typedef long double ld;
-
-int x[MAXN], y[MAXN];
 
 int main() {
   int t;
   scanf("%d", &t);
   for (int tc = 1; tc <= t; tc++) {
     int k, c, s; scanf("%d %d %d", &k, &c, &s);
-    ll pow = k;
-    while(c > 1) { pow = pow * k; c--; }
-    ll step = pow / k;
 
     printf("Case #%d:", tc);
-    for(int i = 0; i < s; i++) {
-      printf(" %lld", step * i + 1);
+    if(s < (k + c - 1) / c) printf(" IMPOSSIBLE");
+    else {
+      for(int i = 0; i < k; i += c) {
+        ll idx = 0;
+        for(int j = 0; j < c; j++) {
+          idx = idx * k + min(i + j, k - 1);
+        }
+        printf(" %lld", idx + 1);
+      }
     }
     printf("\n");
   }
