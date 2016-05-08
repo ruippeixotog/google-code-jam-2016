818a7f82f98ce398b5c932d1987e8cc733b33d46
Rui Gonçalves <ruippeixotog@gmail.com>
2016-05-08 13:48:29 +0100

    Clean up and simplify solution to Senate Evacuation

--- round1c/senate-evacuation.cpp
+++ round1c/senate-evacuation.cpp
@@ -1,75 +1,41 @@
 #include <algorithm>
 #include <cstdio>
-#include <cstring>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <set>
 #include <string>
 #include <utility>
 #include <vector>
 
-#define MAXN 2000
+#define MAXN 26
 
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
-
-int p[MAXN];
-
-vector<pair<int, char>> pairs;
-vector<string> sol;
+pair<int, char> p[MAXN];
 
 int main() {
   int t; scanf("%d", &t);
   for (int tc = 1; tc <= t; tc++) {
-    cerr << "tc" << tc << endl;
     int n; scanf("%d", &n);
 
     int ps = 0;
     for (int i = 0; i < n; i++) {
-      scanf("%d", &p[i]);
-      ps += p[i];
+      scanf("%d", &p[i].first);
+      p[i].second = (char) ('A' + i);
+      ps += p[i].first;
     }
 
-    pairs.clear();
-    for(int i = 0; i < n; i++)
-      pairs.push_back(make_pair(p[i], (char) ('A' + i)));
-
-    sol.clear();
+    vector<string> sol;
     while(ps > 0) {
-      sort(pairs.begin(), pairs.end());
+      sort(p, p + n);
 
-      if(pairs[pairs.size() - 1].first > ps / 2) {
-        fprintf(stderr, "ERROR majority! ps=%d, p[%c]=%d\n", ps,
-                pairs[pairs.size() - 1].second, pairs[pairs.size() - 1].first);
-
-        cerr << "current: " << endl;
-        for(auto p : pairs) {
-          cerr << p.first << ": " << p.second << endl;
-        }
-      }
+      string step(1, p[n - 1].second);
+      p[n - 1].first--; ps--;
 
       if(ps % 2 == 1) {
-        char x = pairs[pairs.size() - 1].second;
-        pairs[pairs.size() - 1].first--;
-
-        sol.push_back(string(1, x));
-        ps--;
-
-      } else {
-        char x1 = pairs[pairs.size() - 1].second;
-        pairs[pairs.size() - 1].first--;
-        char x2 = pairs[pairs.size() - 2].second;
-        pairs[pairs.size() - 2].first--;
-
-        sol.push_back(string(1, x1) + x2);
-        ps -= 2;
+        step.push_back(p[n - 2].second);
+        p[n - 2].first--; ps--;
       }
+      sol.push_back(step);
     }
-
-    if(ps != 0) cerr << "ERROR: " << ps << endl;
 
     printf("Case #%d:", tc);
     for(string s : sol) cout << " " << s;
