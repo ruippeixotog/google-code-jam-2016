fe0f36e946784fa292db3756f679036d4bb7615a
Rui Gonçalves <ruippeixotog@gmail.com>
2016-06-13 01:01:26 +0100

    Clean up "...we did it again" solution

--- distrib-round2/again.cpp
+++ distrib-round2/again.cpp
@@ -1,32 +1,21 @@
-#include <algorithm>
 #include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <utility>
-#include <vector>
 
 #include "again.h"
 #include "message.h"
 
-#define MAXN 2000
+#define MAX_NODES 20
 #define MOD 1000000007
 
 using namespace std;
 
 typedef long long ll;
-typedef long double ld;
 
-ll a[20], b[20];
+ll a[MAX_NODES], b[MAX_NODES];
 
 int main() {
-  int nodeBegin = (MyNodeId() * GetN() / NumberOfNodes());
-  int nodeEnd = ((MyNodeId() + 1) * GetN() / NumberOfNodes());
+  int nodeBegin = (int) (MyNodeId() * GetN() / NumberOfNodes());
+  int nodeEnd = (int) ((MyNodeId() + 1) * GetN() / NumberOfNodes());
 
-//  ll a = 0, b = 0;
   for(int i = nodeBegin; i < nodeEnd; i++) {
     int k = i % NumberOfNodes();
     a[k] = (a[k] + GetA(i)) % MOD;
@@ -43,17 +32,17 @@ int main() {
   } else {
     for(int k = 1; k < NumberOfNodes(); k++) {
       Receive(k);
-      for(int k2 = 0; k2 < NumberOfNodes(); k2++) {
-        a[k2] = (a[k2] + GetLL(k)) % MOD;
-        b[k2] = (b[k2] + GetLL(k)) % MOD;
+      for(int i = 0; i < NumberOfNodes(); i++) {
+        a[i] = (a[i] + GetLL(k)) % MOD;
+        b[i] = (b[i] + GetLL(k)) % MOD;
       }
     }
 
     ll res = 0;
-    for(int k = 0; k < NumberOfNodes(); k++) {
-      for(int k2 = 0; k2 < NumberOfNodes(); k2++) {
-        if((k + k2) % NumberOfNodes() > 0) {
-          res = (res + ((a[k] * b[k2]) % MOD)) % MOD;
+    for(int i = 0; i < NumberOfNodes(); i++) {
+      for(int j = 0; j < NumberOfNodes(); j++) {
+        if((i + j) % NumberOfNodes() > 0) {
+          res = (res + (a[i] * b[j] % MOD)) % MOD;
         }
       }
     }
